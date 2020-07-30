4ba43acd279d37d65fb18c9bd88b4e57e1542aac
Abhijeet <asingh9829@gmail.com>
2020-07-30 16:52:31 +0530

    added schema reusability

--- server/schema.js
+++ server/schema.js
@@ -1,66 +1,76 @@
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLList } = require('graphql')
-const { pubsub } = require('./subscription')
-const fetch = require('cross-fetch')
+const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLList} = require('graphql');
+const {PubSub} = require('graphql-subscriptions');
+const fetch = require('cross-fetch');
+const pubsub = new PubSub();
 const messageType = new GraphQLObjectType({
-    name: 'message',
-    fields: {
-        id: {
-            type: GraphQLString
-        },
-        username: {
-            type: GraphQLString
-        },
-        body: {
-            type: GraphQLString
-        }
-    }
-})
+  name: 'message',
+  fields: {
+    id: {
+      type: GraphQLString,
+    },
+    username: {
+      type: GraphQLString,
+    },
+    body: {
+      type: GraphQLString,
+    },
+  },
+});
 
 const RootQueryType = new GraphQLObjectType({
-    name: 'RootQuery',
-    fields: {
-        messages: {
-            type: GraphQLList(messageType),
-            async resolve(parentValue, args) {
-                const response = await fetch('http://localhost:3003/messages')
-                const data = await response.json()
-                return data
-            }
-        }
-    }
-})
+  name: 'RootQuery',
+  fields: {
+    messages: {
+      type: GraphQLList(messageType),
+      async resolve(parentValue, args) {
+        const response = await fetch('http://localhost:3003/messages');
+        const data = await response.json();
+        return data;
+      },
+    },
+  },
+});
 
 const addMessage = {
-    type: messageType,
-    args: {
-        username: { type: GraphQLString },
-        body: { type: GraphQLString }
-    },
-    resolve: async function (parentValue, args) {
-        const response = await fetch('http://localhost:3003/messages', {
-            method: 'POST',
-            body: JSON.stringify(args),
-            headers: { "Content-Type": "application/json" }
-
-        })
-        const data = await response.json();
-        console.log(data)
-        pubsub.publish('newMessage', { newMessage: data })
-        return data
-    }
-}
-
+  type: messageType,
+  args: {
+    username: {type: GraphQLString},
+    body: {type: GraphQLString},
+  },
+  resolve: async function (parentValue, args) {
+    const response = await fetch('http://localhost:3003/messages', {
+      method: 'POST',
+      body: JSON.stringify(args),
+      headers: {'Content-Type': 'application/json'},
+    });
+    const data = await response.json();
+    console.log(data);
+    pubsub.publish('newMessage', {newMessage: data});
+    return data;
+  },
+};
 
 const MutationType = new GraphQLObjectType({
-    name: "Mutation",
-    fields: {
-        addMessage: addMessage
-    }
-})
+  name: 'Mutation',
+  fields: {
+    addMessage: addMessage,
+  },
+});
 
+const SubscriptionType = new GraphQLObjectType({
+  name: 'Subscription',
+  fields: () => ({
+    newMessage: {
+      type: messageType,
+
+      subscribe: () => pubsub.asyncIterator('newMessage'),
+    },
+  }),
+});
 const schema = new GraphQLSchema({
-    query: RootQueryType,
-    mutation: MutationType
-})
+  query: RootQueryType,
+  mutation: MutationType,
+  subscription: SubscriptionType,
+});
 
-module.exports = schema
+module.exports = schema;
